a8b97e7dda7710970d0ce2f18f101d22be916139
Jarcos09 <168046539+Jarcos09@users.noreply.github.com>
2025-10-13 16:16:50 -0600

    Update features.py

--- MLFlow/DVC/features.py
+++ MLFlow/DVC/features.py
@@ -4,18 +4,29 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from MLFlow/DVC.config import PROCESSED_DATA_DIR
+from MLFlow.DVC.config import PROCESSED_DATA_DIR, INTERIM_DATA_DIR
+
+
+
+FILENAME_INTERIM = "energy_modified_clean.csv"
+FILENAME_PROCESSED = "energy_modified_processed.csv"
 
 app = typer.Typer()
+
+
 
 
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
+    input_path: Path = FILENAME_INTERIM / FILENAME_INTERIM,
+    output_path: Path = PROCESSED_DATA_DIR / FILENAME_PROCESSED,
     # -----------------------------------------
 ):
+
+    print(input_path)
+
+
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating features from dataset...")
     for i in tqdm(range(10), total=10):
