9b7fab951e0db1e86449e1022de8c1ce5025ac77
Jarcos09 <168046539+Jarcos09@users.noreply.github.com>
2025-10-13 11:20:52 -0600

    Update config.py

--- MLFlow/DVC/config.py
+++ MLFlow/DVC/config.py
@@ -11,9 +11,9 @@ PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
 DATA_DIR = PROJ_ROOT / "data"
-RAW_DATA_DIR = DATA_DIR / "raw"
+RAW_DATA_DIR = Path.home() / "MLops_E38_F2" / "data" / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
-PROCESSED_DATA_DIR = DATA_DIR / "processed"
+PROCESSED_DATA_DIR = Path.home() / "MLops_E38_F2" / "data" / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
 MODELS_DIR = PROJ_ROOT / "models"
